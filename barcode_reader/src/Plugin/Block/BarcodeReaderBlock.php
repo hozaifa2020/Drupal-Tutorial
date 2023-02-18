ef602a791e9cc2ca12bc8c5f0d60af91ea0dfeb2
Abu Hozaifa <abuhozaifa8@gmail.com>
2023-02-18 09:15:35 +0530

    Display barcode for product type only

--- barcode_reader/src/Plugin/Block/BarcodeReaderBlock.php
+++ barcode_reader/src/Plugin/Block/BarcodeReaderBlock.php
@@ -18,23 +18,23 @@ class BarcodeReaderBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-	  
     $build = [];
     $build['#theme'] = 'barcode_reader_block';
     $node = \Drupal::routeMatch()->getParameter('node');
+	if($node->bundle() == 'product') {
    if ($node instanceof \Drupal\node\NodeInterface) {
       $nid = $node->id();
 	  $node = \Drupal\node\Entity\Node::load($nid);
 	  $product_url = $node->field_product_url->uri;
     }	
-	$build['#prefix'] = 'To Purchase this product on your app to avail exclusive app-only';
+	  $build['#prefix'] = 'To Purchase this product on your app to avail exclusive app-only';
      $build['barcode_reader_block']['#markup'] = "
 						<img src='https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=$product_url&choe=UTF-8'>";
 
-    return $build;
+       return $build;
 	
+	}
   }
-  
    public function getCacheMaxAge() {
     return 0;
    }
