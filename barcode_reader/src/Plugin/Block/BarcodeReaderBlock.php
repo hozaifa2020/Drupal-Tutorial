124225cb2be9386d3a7af9b2973975ab27d456d6
Abu Hozaifa <abuhozaifa8@gmail.com>
2023-02-19 11:11:44 +0530

    Barcode Reader Module

--- barcode_reader/src/Plugin/Block/BarcodeReaderBlock.php
+++ barcode_reader/src/Plugin/Block/BarcodeReaderBlock.php
@@ -20,20 +20,23 @@ class BarcodeReaderBlock extends BlockBase {
   public function build() {
     $build = [];
     $build['#theme'] = 'barcode_reader_block';
+    $url = '';
     $node = \Drupal::routeMatch()->getParameter('node');
-	if($node->bundle() == 'product') {
-   if ($node instanceof \Drupal\node\NodeInterface) {
-      $nid = $node->id();
-	  $node = \Drupal\node\Entity\Node::load($nid);
-	  $product_url = $node->field_product_url->uri;
-    }	
+      if ($node instanceof \Drupal\node\NodeInterface) {
+      if($node->hasField('field_product_url') && !$node->get('field_product_url')->isEmpty()){
+        $url = $node->get('field_product_url')->getValue()[0]['uri'];
+      }
+
+    }
+
+
+	
 	  $build['#prefix'] = 'To Purchase this product on your app to avail exclusive app-only';
      $build['barcode_reader_block']['#markup'] = "
-						<img src='https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=$product_url&choe=UTF-8'>";
+						<img src='https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=$url&choe=UTF-8'>";
 
        return $build;
 	
-	}
   }
    public function getCacheMaxAge() {
     return 0;
